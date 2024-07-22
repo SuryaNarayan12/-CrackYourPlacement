b9e96358a1a38a2e364f8e870974b8b9a320356f
Surya Narayan <148715825+SuryaNarayan12@users.noreply.github.com>
2024-07-22 14:44:27 +0530

    Create Rabin-Karp Algorithm for Pattern Searching.java

---
+++ String/Rabin-Karp Algorithm for Pattern Searching.java
@@ -0,0 +1,44 @@
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        // your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int q = 101;
+        int d = 256;
+        int M = pat.length();
+        int N = txt.length();
+        int i, j;
+        int p = 0; 
+        int t = 0;
+        int h = 1;
+ 
+        for (i = 0; i < M - 1; i++)
+            h = (h * d) % q;
+ 
+        for (i = 0; i < M; i++) {
+            p = (d * p + pat.charAt(i)) % q;
+            t = (d * t + txt.charAt(i)) % q;
+        }
+        for (i = 0; i <= N - M; i++) {
+            if (p == t) {
+                for (j = 0; j < M; j++) {
+                    if (txt.charAt(i + j) != pat.charAt(j))
+                        break;
+                }
+                if (j == M)
+                    ans.add(i+1);
+            }
+            if (i < N - M) {
+                t = (d * (t - txt.charAt(i) * h)
+                     + txt.charAt(i + M))
+                    % q;
+
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+        return ans;
+    }
+}
