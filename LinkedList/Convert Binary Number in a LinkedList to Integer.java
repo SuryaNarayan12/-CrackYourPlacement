cb6530a884b5552ecf41c67c02570bd33588be5e
Surya Narayan <148715825+SuryaNarayan12@users.noreply.github.com>
2024-07-25 12:56:28 +0530

    Create Convert Binary Number in a LinkedList to Integer.java

---
+++ LinkedList/Convert Binary Number in a LinkedList to Integer.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        head = reverse(head);
+        int i = 0, ans = 0;
+        while(head != null){
+            ans += head.val * (int)Math.pow(2,i++);
+            head = head.next;
+        }
+        return ans;
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode prev = null, curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
